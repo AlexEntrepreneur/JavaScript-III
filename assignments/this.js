225221aa0eede244d1cc13cf4c5a74a61e74a086
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-01-23 14:03:18 +0000

    Complete This Principle 2 Code Example

--- assignments/this.js
+++ assignments/this.js
@@ -19,6 +19,16 @@ defaultBinding();
 // code example for Window Binding
 
 // Principle 2
+function implicitBinding() {
+  console.log(`Implicit Binding: \"this\" refers to => \"${this.name}\" Object`);
+}
+
+const implicitObject = {
+  "name": "implicitObject",
+  "where_is_this": implicitBinding
+}
+
+implicitObject.where_is_this();
 
 // code example for Implicit Binding
 
